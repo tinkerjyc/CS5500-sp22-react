8964004fb2cf3017e00221a6d9917bfd3a537e79
jyc <tinkerjyc@gmail.com>
2022-04-02 14:26:26 -0400

    Fix remote bug

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-const TUITS_API = "https://cs5500-a4-node.herokuapp.com/api/tuits";
-const USERS_API = "https://cs5500-a4-node.herokuapp.com/api/users";
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+const USERS_API = `${BASE_URL}/api/users`;
+const TUITS_API = `${BASE_URL}/api/tuits`;
 
 export const findAllTuits = () =>
     axios.get(TUITS_API)
